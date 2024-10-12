c129839928122bd94d11fd74415f06b2793a694e
thien2218 <thien1367526@gmail.com>
2024-10-12 13:40:51 +0700

    Move app routes to /routes directory

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,34 @@
-export { default as authRoutes } from "./auth.route";
-export { default as postRoutes } from "./post.route";
-export { default as userRoutes } from "./user.route";
-export { default as uploadRoutes } from "./upload.route";
-export { default as commentRoutes } from "./comment.route";
-export { default as draftRoutes } from "./draft.route";
-export { default as seriesRoutes } from "./series.route";
-export { default as tagRoutes } from "./tag.route";
-export { default as reactionRoutes } from "./reaction.route";
+import { AppEnv } from "@/context";
+import { auth, session, validateResponse } from "@/middlewares";
+import { Hono } from "hono";
+import { csrf } from "hono/csrf";
+import authRoutes from "./auth.route";
+import postRoutes from "./post.route";
+import userRoutes from "./user.route";
+import uploadRoutes from "./upload.route";
+import commentRoutes from "./comment.route";
+import seriesRoutes from "./series.route";
+import draftRoutes from "./draft.route";
+import tagRoutes from "./tag.route";
+import reactionRoutes from "./reaction.route";
+
+export const app = new Hono<AppEnv>().basePath("/api");
+
+/* Middlewares */
+app.use(csrf({ origin: "http://localhost:8787" }));
+app.use(session);
+app.use(validateResponse);
+app.put("*", auth);
+app.patch("*", auth);
+app.delete("*", auth);
+
+/* Routes */
+app.route("/", authRoutes);
+app.route("/", postRoutes);
+app.route("/", userRoutes);
+app.route("/", uploadRoutes);
+app.route("/", commentRoutes);
+app.route("/", seriesRoutes);
+app.route("/", draftRoutes);
+app.route("/", tagRoutes);
+app.route("/", reactionRoutes);
